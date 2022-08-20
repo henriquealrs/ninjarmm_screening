f41eef4a50a46412441b76bcbce0265585a61869
henriquealrs <henriquealrs@gmail.com>
2022-08-20 10:00:59 -0300

    step 5

--- step5/CodeChallenge.cpp
+++ step5/CodeChallenge.cpp
@@ -36,10 +36,11 @@ OBJ_STR objStr;
 //Here's a template function that can accept a sample object and 3 integer parameters. It checks if the sum of the integer params is positive or not.
 //The third integer param is optional so it's declared as a default param
 //Inside the function is some existing code that needs to exist, but not relevant to this exercise. 
-template <typename T, typename U>
-T checkIfPositive(const U& u, const int& param1, const int& param2, int param3 = 0)
-{
+static int TOTAL_IGNORE = 0;
 
+template <typename T, typename U>
+T checkIfPositive(const U& u, const int& param1, const int& param2, int param3 = 0, int& total = TOTAL_IGNORE)
+{
     // Assume there's already some existing code in here, some business logic that we are not interested in this exercise. 
     /*
 
@@ -47,6 +48,7 @@ T checkIfPositive(const U& u, const int& param1, const int& param2, int param3 =
 
     */
 
+    total = param1 + param2 + param3;
     return u.getResult(param1, param2, param3);
 }
 
@@ -75,10 +77,10 @@ int main()
 
     //Now here's a new requirement where you need to get the total as an output param of checkIfPositive() template function
     //You can't touch the previous calls to checkIfPositive because they are used in a lot of different places throughout the code base.
-    //How are you going to solve this problem? 
+    //How are you going to solve this problem?
     //You can only change the code inside the EDITABLE SECTION OF CODE
     //Hint, the following calls should now be valid moving forward:
-    /*
+
     int total = 0;
 
     iResult = checkIfPositive<int, OBJ_INT>(objInt, 1, 2);
@@ -98,7 +100,6 @@ int main()
 
     sResult = checkIfPositive<std::string, OBJ_STR>(objStr, 1, 2, 3, total); //<-- Total is output param
     std::cout << sResult << " " << total << std::endl;
-    */
 
     return 0;
 
